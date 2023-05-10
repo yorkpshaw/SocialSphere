8069578f36d8aeb4a7c0e250c90d5c672966b485
York Shaw <york.p.shaw@gmail.com>
2023-05-10 11:03:13 -0500

    login page/form

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Socialsphere
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
