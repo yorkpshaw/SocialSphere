9536bc0f2cf5b97203fa918ca542cd5ca0c0cfe5
York Shaw <york.p.shaw@gmail.com>
2023-06-02 12:48:40 -0500

    Fixed useMediaQuery 1000px

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -3,7 +3,7 @@ import Form from "./Form";
 
 const LoginPage = () => {
     const theme = useTheme();
-    const isNonMobileScreens = useMediaQuery("(min-width: 1000 px)");
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
     return (
     <Box>
